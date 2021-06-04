8cb22b6fecbc6f9270ff5f0719b993dd5ffcbdd4
mohamedslam <mohamedslam2000@yahoo.com>
2021-06-04 14:31:28 +0500

    Add project files.

---
+++ AirTickets_CSU_2021/__init__.py
@@ -0,0 +1,3 @@
+"""
+Package for AirTickets_CSU_2021.
+"""
---
+++ env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\msallam\Desktop\OS\MyFiles\Airtickets_CSU_2021\AirTickets_CSU_2021\env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
